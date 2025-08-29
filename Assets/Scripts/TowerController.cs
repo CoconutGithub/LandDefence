1b39adbcaa431958e2e51cf45cccb0813c0cd4ce
Coconut <khkoh5932@gmail.com>
2025-08-29 14:11:40 +0900

    폭탄 유도 제거, 범위 내 피해 유지

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -32,6 +32,13 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private float timeBetweenShots = 0.1f;
     
+    [Header("레이저 정보 (영국 마법사 전용)")]
+    public bool isLaserTower = false;
+    [SerializeField]
+    private float laserDps = 30f;
+    [SerializeField]
+    private float laserDpsRamp = 10f;
+    
     private float finalProjectileDamage;
 
     [Header("업그레이드 정보")]
@@ -42,15 +49,24 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
     private GameObject projectilePrefab;
     [SerializeField]
     private Transform firePoint;
-
+    [SerializeField]
+    private LineRenderer laserLineRenderer;
+    
     private Transform currentTarget;
     private float attackCountdown = 0f;
     private TowerSpotController parentSpot;
+    private float currentDpsRamp = 0f;
+    private Transform lastTarget;
 
     void Start()
     {
         int damageLevel = DataManager.LoadDamageLevel(towerType);
         finalProjectileDamage = baseProjectileDamage * (1f + (damageLevel * 0.1f));
+
+        if (isLaserTower && laserLineRenderer != null)
+        {
+            laserLineRenderer.enabled = false;
+        }
     }
 
     public void SetParentSpot(TowerSpotController spot)
@@ -60,7 +76,6 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        // (수정) 통합된 TowerUpgradeUI를 호출합니다.
         if (upgradePaths != null && upgradePaths.Length > 0)
         {
             TowerUpgradeUI.instance.Show(this);
@@ -84,10 +99,49 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
         FindClosestEnemy();
         attackCountdown -= Time.deltaTime;
 
-        if (currentTarget != null && attackCountdown <= 0f)
+        if (isLaserTower)
         {
-            StartCoroutine(AttackBurst());
-            attackCountdown = timeBetweenAttacks;
+            HandleLaserAttack();
+        }
+        else
+        {
+            if (currentTarget != null && attackCountdown <= 0f)
+            {
+                StartCoroutine(AttackBurst());
+                attackCountdown = timeBetweenAttacks;
+            }
+        }
+    }
+
+    void HandleLaserAttack()
+    {
+        if (currentTarget != null)
+        {
+            laserLineRenderer.enabled = true;
+            laserLineRenderer.SetPosition(0, firePoint.position);
+            laserLineRenderer.SetPosition(1, currentTarget.position);
+
+            if (currentTarget == lastTarget)
+            {
+                currentDpsRamp += laserDpsRamp * Time.deltaTime;
+            }
+            else
+            {
+                currentDpsRamp = 0f;
+            }
+            lastTarget = currentTarget;
+
+            float totalDps = laserDps + currentDpsRamp;
+            currentTarget.GetComponent<EnemyHealth>().TakeDamage(totalDps * Time.deltaTime, towerType, damageType);
+        }
+        else
+        {
+            if (laserLineRenderer.enabled)
+            {
+                laserLineRenderer.enabled = false;
+                currentDpsRamp = 0f;
+                lastTarget = null;
+            }
         }
     }
 
@@ -95,7 +149,10 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
     {
         for (int i = 0; i < bulletsPerBurst; i++)
         {
-            if (currentTarget == null) yield break;
+            if (currentTarget == null)
+            {
+                yield break;
+            }
             Shoot();
             yield return new WaitForSeconds(timeBetweenShots);
         }
@@ -109,12 +166,19 @@ public class TowerController : MonoBehaviour, IPointerClickHandler
         if (towerType == TowerType.Bomb)
         {
             BombProjectileController bomb = projectileGO.GetComponent<BombProjectileController>();
-            if (bomb != null) bomb.Setup(currentTarget, finalProjectileDamage, projectileSpeed, explosionRadius, towerType, damageType);
+            if (bomb != null)
+            {
+                // (수정) 목표물(Transform) 대신 목표물의 현재 위치(Vector3)를 전달합니다.
+                bomb.Setup(currentTarget.position, finalProjectileDamage, projectileSpeed, explosionRadius, towerType, damageType);
+            }
         }
         else
         {
             ProjectileController projectile = projectileGO.GetComponent<ProjectileController>();
-            if (projectile != null) projectile.Setup(currentTarget, finalProjectileDamage, projectileSpeed, towerType, damageType, slowAmount, slowDuration);
+            if (projectile != null)
+            {
+                projectile.Setup(currentTarget, finalProjectileDamage, projectileSpeed, towerType, damageType, slowAmount, slowDuration);
+            }
         }
     }
 
